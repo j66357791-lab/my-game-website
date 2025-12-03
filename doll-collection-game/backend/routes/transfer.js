b2bd1c87834ae40b4c1833a836879d821f978acc
j66357791-lab <j66357791@gmail.com>
2025-12-03 18:17:36 +0800

    Delete doll-collection-game directory

--- doll-collection-game/backend/routes/transfer.js
+++
@@ -1,9 +0,0 @@
-// routes/transfer.js
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/auth');
-const transferController = require('../controllers/transferController');
-
-router.post('/', auth, transferController.processTransfer);
-
-module.exports = router;
