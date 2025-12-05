10d2a8b2dd51d62611ec289938bcb3b14e6dea2d
j66357791-lab <j66357791@gmail.com>
2025-12-06 01:19:44 +0800

    Update index.js

--- chaowan-platform/frontend/src/index.js
+++ chaowan-platform/frontend/src/index.js
@@ -1,20 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <link rel="icon" href="%PUBLIC_URL%/favicon.ico?v=20241201" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="theme-color" content="#000000" />
-    <meta
-      name="description"
-      content="Web site created using create-react-app"
-    />
-    <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png?v=20241201" />
-    <link rel="manifest" href="%PUBLIC_URL%/manifest.json?v=20241201" />
-    <title>娃娃养成平台</title>
-  </head>
-  <body>
-    <noscript>You need to enable JavaScript to run this app.</noscript>
-    <div id="root"></div>
-  </body>
-</html>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
