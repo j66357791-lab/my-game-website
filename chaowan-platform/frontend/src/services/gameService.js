01b112bf6fef634716d6cd6e07f198fb4567894a
j66357791-lab <j66357791@gmail.com>
2025-12-11 11:24:43 +0800

    Delete chaowan-platform/frontend/src/services directory

--- chaowan-platform/frontend/src/services/gameService.js
+++
@@ -1,9 +0,0 @@
-// src/services/gameService.js
-import api from './api';
-
-export default {
-  getCurrentGame: () => api.get('/game/current'),
-  placeBet: (data) => api.post('/game/bet', data),
-  getHistory: (params) => api.get('/game/history', { params }),
-  getStats: () => api.get('/game/stats')
-};
