d1a9378f259b2b4d6b277c512211270bc7ecc04f
j66357791-lab <j66357791@gmail.com>
2025-12-29 20:44:26 +0800

    Delete chaowan-platform/backend directory

--- chaowan-platform/backend/routes/mail.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect } = require('../middleware/auth');
-const { getUserMails, claimMail } = require('../controllers/mailController');
-
-router.route('/').get(protect, getUserMails);
-router.route('/:mailId/claim').post(protect, claimMail);
-
-module.exports = router;
