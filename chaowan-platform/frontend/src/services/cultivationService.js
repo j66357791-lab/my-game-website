716bfd1378f1a3fce7410312f0ed4da0e9d8817d
j66357791-lab <j66357791@gmail.com>
2025-12-30 20:22:42 +0800

    Delete chaowan-platform/frontend/src/services directory

--- chaowan-platform/frontend/src/services/cultivationService.js
+++
@@ -1,9 +0,0 @@
-import api from '../config/api';
-
-export const cultivationService = {
-  getData: () => api.get('/cultivation/data'),
-  allocatePoint: (attr) => api.post('/cultivation/allocate', { attr }),
-  claim: () => api.post('/cultivation/claim'),
-  breakthrough: (pill) => api.post('/cultivation/breakthrough', { usePillQuality: pill }),
-  // ...装备相关接口
-};
